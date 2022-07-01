744aebc01d99fda02e794a91033f4b7b1d74768a
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-07-01 12:31:55 -0700

    My commit task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Return: Always 0 (Success/Currect)
+*/
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+continue;
+else is (alphabet == 'e')
+continue;
+putchar(alphabet);
+}
+putchar('\n);
+return (0)
+}
