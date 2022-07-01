b5d68efa4d4ab50c29f77f514c5284b3fb7bbc23
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-07-01 10:03:13 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Energy point
+*
+* Return: Always 0 (success/correct)
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n % 10 > 5)
+{
+printf("Last digit of %1 and is greater than 5/n", n, n % 10);
+}
+else if (n % 10 == 0)
+{
+printf("Last digit of %1 and is 0\n", n, n % 10);
+}
+else
+{
+printf("Last digit of %1 and is less than 6 and not 0\n", n, n % 10);
+}
+return (0)
+}
