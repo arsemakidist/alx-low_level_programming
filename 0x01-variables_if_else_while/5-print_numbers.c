591645b6a28cf87dc1b638471d4df96cf8978198
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-07-01 13:05:48 -0700

    My commit task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success/Correct)
+*/
+int main(void)
+{
+int i;
+for (i = 0 ; 1 < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
+}
