add2a13bb4be8fe0de18cdd08d53da446aa7502b
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-07-01 05:27:59 -0700

    My commit task 6 c pro

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
 /**
-* main - prints the size of various types
-* Return: 0 if exited properly, non-zero otherwise
+* main - Entry point
+*
+* Return: always 0 (Success)
 */
 int main(void)
 {
-printf("size of a char: 1 byte(s)", sizeof(char));
-printf("Size of an int: 4 byte(s)", sizeof(int));
-printf("Size of a long int: 8 byte(s)", sizeof(long int));
-printf("Size of a long long int: 8 byte(s)", sizeof(long long int));
-printf("Size of a float: 4 byte(s)", sizeof(float));
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
