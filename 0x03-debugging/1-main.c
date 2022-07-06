578013d40524978b41e8e85728ebc9042cac2a7b
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-07-06 02:50:30 -0700

    My commit task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,11 +11,10 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 /*
 *while (i < 10)
-*{
-*Infinite loop - No increase of variable
-*putchar(i);
-}
+*	{
+* putchar(i);
+	}
 */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
-}
+
