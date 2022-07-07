d779a3c87200a95135b2966b278f885eb00ba702
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-07-07 07:43:49 -0700

    commit task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * main - tests function that prints if integer is positive or negative
 *
@@ -8,6 +9,6 @@ int main(void)
 {
 int i;
 i = 0;
-positive_or_negative(i);
+positive_or_negative(int i);
 return (0);
 }
